746f52a38630b544951dbdc282f5ff798d85e271
duongbaphuc1@gmail.com <phuc1234>
2014-10-15 04:02:30 +0700

    alter table user

--- data/users.sql
+++ data/users.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 23, 2014 at 06:28 AM
+-- Generation Time: Oct 15, 2014 at 03:58 AM
 -- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.3
+-- PHP Version: 5.5.9-1ubuntu4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `mariland`
+-- Database: `newcanho`
 --
 
 -- --------------------------------------------------------
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(32) NOT NULL,
   `email` varchar(250) CHARACTER SET latin1 NOT NULL,
   `full_name` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `role` varchar(10) NOT NULL,
   `createdby` int(11) NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -41,10 +42,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `full_name`, `createdby`, `created_date`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'duongbaphuc@gmail.com', 'admin', 0, '0000-00-00 00:00:00'),
-(2, 'phuc', '886d057a091559e2f5dff95d1d01360b', 'phuc.duong@kiss-concept.com', 'Mr Phuc', 0, '2013-06-17 13:55:19'),
-(3, 'admin1', '123456', '1duongbaphuc@gmail.com', 'admin', 1, '2013-06-22 08:29:22');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `full_name`, `role`, `createdby`, `created_date`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'duongbaphuc@gmail.com', 'admin', '', 0, '0000-00-00 00:00:00'),
+(2, 'phuc', '886d057a091559e2f5dff95d1d01360b', 'phuc.duong@kiss-concept.com', 'Mr Phuc', '', 0, '2013-06-17 13:55:19'),
+(3, 'admin1', '123456', '1duongbaphuc@gmail.com', 'admin', '', 1, '2013-06-22 08:29:22');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
